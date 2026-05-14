aec71469c3d8e7cac9c0f3094e5249a4cb04dc92
JekaTSen <kuharjeka@gmail.com>
2026-05-14 12:00:05 +0200

    vesting.sol modified (added some functionality)

--- contracts/LinearVesting/Vesting.sol
+++ contracts/LinearVesting/Vesting.sol
@@ -10,13 +10,20 @@ contract Vesting is IUtilityContract, Ownable {
     constructor() Ownable (msg.sender) {}
 
     IERC20 public token;
-    address public beneficiary; //3. Vesting smart contract can be used by multiple people.  MerkleTree or Mapping (both is good)
     uint256 public totalAmount;
+    
+    address public beneficiary; 
+    //mapping(address => uint256) public beneficiaries; //3. Vesting smart contract can be used by multiple people.  MerkleTree or Mapping (both is good)
+
+
     uint256 public startTime;
     uint256 public cliff;
     uint256 public duration;
-    uint256 public claimed;
 
+    uint256 public claimed;
+    uint256 public cooldown;
+
+    bool public vestingStarted;
 
     bool public initialized;
 
@@ -27,8 +34,13 @@ contract Vesting is IUtilityContract, Ownable {
     error CliffNotReached();
     error NothingToClaim();
     error TranfserFailed();
+    error Cooldown();
+    error MinClaimAmount();
+    error NotEnoughTokensOnContract();
+    error vestingWasntStartedYet();
 
     event Claimed(address beneficiary, uint256 amount, uint256 timestamp);
+    event VestingStarted(uint256 timestamp);
 
     modifier notInitialized() {
         require(!initialized, AlreadyInitialized());
@@ -36,26 +48,37 @@ contract Vesting is IUtilityContract, Ownable {
     }
     
 
-    //4. split initialization logic and start vesting logic.
-    //initialization = tokenAddress, totalAmount, owner
-    //startVesting (arg1, arg2, ...) require tokens on contract 
 
-    //5. initialize, getIniitData should work.
+
+    function startVesting(uint256 _cliff, uint256 _duration) external onlyOwner {
+        require(token.balanceOf(address(this)) >= totalAmount, NotEnoughTokensOnContract());
+        
+        cliff = _cliff;
+        duration = _duration;
+        startTime = block.timestamp;
+
+        //return true;
+        vestingStarted = true;
+
+        emit VestingStarted(block.timestamp);
+    }
 
 
     function claim() public {
-        //1. add claim cooldown --> 
-        //2. add min claim amount
-        require(msg.sender == beneficiary, ClaimerIsNotBeneficiary());
+        require(vestingStarted, vestingWasntStartedYet());
         require(block.timestamp > startTime + cliff, CliffNotReached());
+        require(cooldown < block.timestamp, Cooldown()); //cooldown if u claimed, have to wait 1 day
+        require(msg.sender == beneficiary, ClaimerIsNotBeneficiary());
 
         uint256 claimable = claimableAmount();
         require(claimable > 0, NothingToClaim());
+        require(claimable > 100, MinClaimAmount()); //min 100
 
         claimed += claimable;
         (bool success) = token.transfer(beneficiary, claimable);
         require(success, TranfserFailed());
 
+        cooldown = block.timestamp + 1 days;
         emit Claimed(msg.sender, claimable, block.timestamp);
     }
 
@@ -78,9 +101,10 @@ contract Vesting is IUtilityContract, Ownable {
 
     function initialize(bytes memory _initData) external notInitialized returns (bool) {
 
-        (address _token, address _treasury, address _owner) = abi.decode(_initData, (address, address, address));
+        (address _token, uint256 _totalAmount, address _owner) = abi.decode(_initData, (address, uint256, address));
 
-
+        token = IERC20(_token);
+        totalAmount = _totalAmount;
         Ownable.transferOwnership(_owner);
 
         initialized = true;
@@ -88,8 +112,8 @@ contract Vesting is IUtilityContract, Ownable {
     }
 
 
-    function getInitData(address _token, address _treasury, address _owner) external pure returns (bytes memory){
-        return abi.encode(_token, _treasury, _owner);
+    function getInitData(address _token, uint256 _totalAmount, address _owner) external pure returns (bytes memory){
+        return abi.encode(_token, _totalAmount, _owner);
     }
 
 
